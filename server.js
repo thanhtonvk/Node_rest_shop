de96a0743bde7d41bb5df405a2f8bdde4c0b6093
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2022-02-19 11:10:43 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const productRoutes = require("./api/routes/product");
 const orderRoutes = require("./api/routes/orders");
 const bodyParser = require("body-parser");
 
 var mongoose = require("mongoose");
 mongoose.connect(
- "mongodb+srv://NodeShop:pKNQH4Rr9e2FQVi@nodeshop.xcj7t.mongodb.net/nodeshop?retryWrites=true&w=majority",
- {
-   useUnifiedTopology:true,
-   useNewUrlParser:true
- }
+    "mongodb+srv://NodeShop:pKNQH4Rr9e2FQVi@nodeshop.xcj7t.mongodb.net/nodeshop?retryWrites=true&w=majority", {
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    }
 );
 
 app.use(morgan("dev"));
 
 app.use(
-  bodyParser.urlencoded({
-    extended: false,
-  })
+    bodyParser.urlencoded({
+        extended: false,
+    })
 );
 
 app.use(bodyParser.json());
@@ -29,33 +28,33 @@ app.use("/product", productRoutes);
 app.use("/orders", orderRoutes);
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  if (res.method === "OPTION") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE");
-    return res.status(200).json({});
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+    );
+    if (res.method === "OPTION") {
+        res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE");
+        return res.status(200).json({});
+    }
+    next();
 });
 
 app.use((req, res, next) => {
-  const error = new Error("Not found");
-  error.status = 404;
-  next(error);
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
 });
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
-  res.json({
-    error: {
-      message: error.message,
-    },
-  });
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
 });
 
 app.listen(port, () => {
-  console.log(`Run http://locahost:${port}`);
+    console.log(`Run http://locahost:${port}`);
 });
